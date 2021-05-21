698e908c9a35b8eb6a418a266cb7481650eee04c
pritesh-peter <pritesh.peter@gmail.com>
2021-05-21 22:37:33 +0545

    router redux

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBRS_FBrP-sl5VqGlUU2Q4KuTSRqs3h1Dk",
+    authDomain: "clone-84122.firebaseapp.com",
+    projectId: "clone-84122",
+    storageBucket: "clone-84122.appspot.com",
+    messagingSenderId: "759204995171",
+    appId: "1:759204995171:web:babbcb0771c094657b9856",
+    measurementId: "G-3X7X2Z5LGW"
+  };
