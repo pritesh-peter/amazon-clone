eac04ea800183557b5e63c5bd630d607e5ca5f61
pritesh-peter <pritesh.peter@gmail.com>
2021-05-22 22:17:22 +0545

    authentication done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Login from "./Login";
+import {useStateValue} from "./StateProvider";
+import {auth} from "./firebase";
+
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      console.log("User is >>>>>", authUser);
+
+      if (authUser) {
+        //the use just logged in    the user was logged in
+
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        //the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
   return (
     <Router>
       <div className="App">
--- src/Header.js
+++ src/Header.js
@@ -2,11 +2,20 @@ import React from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
-import {Link} from "react-router-dom";
+import {Link, useHistory} from "react-router-dom";
 import {useStateValue} from "./StateProvider";
+import {auth} from "./firebase";
 
 function Header() {
-  const [{basket}, dispatch] = useStateValue();
+  const [{basket, user}, dispatch] = useStateValue();
+  const history = useHistory();
+
+  const handleAuthentication = () => {
+    if (user) {
+      auth.signOut();
+      history.push("/login");
+    }
+  };
 
   return (
     <div className="header">
@@ -23,10 +32,14 @@ function Header() {
         <SearchIcon className="header__searchIcon" />
       </div>
       <div className="header__nav">
-        <Link to="/login">
-          <div className="header__option">
-            <span className="header__optionsLineOne">Hello Guest</span>
-            <span className="header__optionsLineTwo">Sign In</span>
+        <Link to={!user && "/login"}>
+          <div onClick={handleAuthentication} className="header__option">
+            <span className="header__optionsLineOne">
+              {user ? `Hello ${user.email}` : "Hello User"}
+            </span>
+            <span className="header__optionsLineTwo">
+              {user ? "Sign Out" : "Sign In"}
+            </span>
           </div>
         </Link>
         <div className="header__option">
